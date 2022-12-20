8a5dcc8a006b348c28afa378751400e0d043a4e6
xX-BERTANT-Xx <101613578+xX-BERTANT-Xx@users.noreply.github.com>
2022-12-20 21:37:44 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-let img = document.querySelector('img');
-let left = document.getElementById('left');
-let right = document.getElementById('right');
-let index = 0;
-function show(){
-    img.src = `img/${index}.jpg`;
-}
-function clickPlay(){
-    left.onclick = function(){
-        if(index <= 0){
-            index = span.length - 1;
-        }else{
-            index --;
-        }
-        show();
-    }
-    right.onclick = function(){
-        if(index == span.length){
-            index = 0;
-        }
-        index ++;
-        show();
-    }
-}
-clickPlay();
+var $ = mdui.$;
+$('#dl').on('open.mdui.dialog', function () {
+    tab.handleUpdate();
+  });
